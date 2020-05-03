9450546123a83117d926f0a563690bdc7c7c835f
Andrey Nesterov <andrey.nesterov@netcracker.com>
2020-05-03 15:45:59 +0300

    Add mysql gen script, model; Reformat pg gen script

---
+++ base_dbs/scripts/init/mysql.sql
@@ -0,0 +1,94 @@
+create database if not exists db_itmo;
+
+use db_itmo;
+
+drop table if exists conference_publications;
+drop table if exists person_projects;
+drop table if exists person_publications;
+drop table if exists issue_publications;
+drop table if exists projects;
+drop table if exists issues;
+drop table if exists conferences;
+drop table if exists publications;
+drop table if exists persons;
+
+create table persons
+(
+    person_id       SERIAL,
+    last_name       VARCHAR(60) NOT NULL,
+    first_name      VARCHAR(60) NOT NULL,
+    patronymic_name VARCHAR(60),
+    role            VARCHAR(20) NOT NULL
+);
+
+create table projects
+(
+    project_id SERIAL,
+    name       VARCHAR(60) NOT NULL
+);
+
+create table publications
+(
+    publication_id SERIAL,
+    name           VARCHAR(60) NOT NULL,
+    language       VARCHAR(3)  NOT NULL DEFAULT 'RU',
+    citation_index INT         NOT NULL DEFAULT 0,
+    date           TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+create table issues
+(
+    issue_id SERIAL,
+    name     VARCHAR(60)  NOT NULL,
+    language VARCHAR(3)   NOT NULL DEFAULT 'RU',
+    location VARCHAR(255) NOT NULL,
+    length   INT          NOT NULL,
+    format   VARCHAR(60)  NOT NULL
+);
+
+create table conferences
+(
+    conference_id SERIAL,
+    name          VARCHAR(60)  NOT NULL,
+    location      VARCHAR(255) NOT NULL,
+    date          TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+create table person_publications
+(
+    person_id      BIGINT UNSIGNED NOT NULL,
+    publication_id BIGINT UNSIGNED NOT NULL,
+    PRIMARY KEY (person_id, publication_id),
+    FOREIGN KEY perpub_person_fk (person_id) REFERENCES persons (person_id),
+    FOREIGN KEY perpub_publication_fk (publication_id) REFERENCES publications (publication_id)
+);
+
+create table person_projects
+(
+    person_id           BIGINT UNSIGNED NOT NULL,
+    project_id          BIGINT UNSIGNED NOT NULL,
+    participation_start TIMESTAMP       NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    participation_end   TIMESTAMP       NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (person_id, project_id),
+    FOREIGN KEY perpro_person_fk (person_id) REFERENCES persons (person_id),
+    FOREIGN KEY perpro_project_fk (project_id) REFERENCES projects (project_id)
+);
+
+create table issue_publications
+(
+    issue_id       BIGINT UNSIGNED NOT NULL,
+    publication_id BIGINT UNSIGNED NOT NULL,
+    PRIMARY KEY (issue_id, publication_id),
+    FOREIGN KEY isspub_issue_fk (issue_id) REFERENCES issues (issue_id),
+    FOREIGN KEY isspub_publication_fk (publication_id) REFERENCES publications (publication_id)
+
+);
+
+create table conference_publications
+(
+    conference_id  BIGINT UNSIGNED NOT NULL,
+    publication_id BIGINT UNSIGNED NOT NULL,
+    PRIMARY KEY (conference_id, publication_id),
+    FOREIGN KEY conpub_conference_fk (conference_id) REFERENCES conferences (conference_id),
+    FOREIGN KEY conpub_publication_fk (publication_id) REFERENCES publications (publication_id)
+);
--- base_dbs/scripts/init/pg.sql
+++ base_dbs/scripts/init/pg.sql
@@ -7,58 +7,66 @@ drop table if exists specialties;
 drop table if exists semesters;
 drop table if exists faculties;
 
-create table faculties (
-	faculty_id serial primary key,
-	faculty_name text unique not null
+create table faculties
+(
+    faculty_id   serial primary key,
+    faculty_name text unique not null
 );
 
-create table semesters (
-	semester_id serial primary key
+create table semesters
+(
+    semester_id serial primary key
 );
 
-create table specialties (
-	specialty_id serial primary key,
-	specialty_name text unique not null
+create table specialties
+(
+    specialty_id   serial primary key,
+    specialty_name text unique not null
 );
 
-create table persons (
-	person_id serial primary key,
-	last_name text not null,
-	first_name text not null,
-	patronymic_name text,
-	university_role text not null,
-	faculty_id int not null references faculties (faculty_id)
+create table persons
+(
+    person_id       serial primary key,
+    last_name       text not null,
+    first_name      text not null,
+    patronymic_name text,
+    university_role text not null,
+    faculty_id      int  not null references faculties (faculty_id)
 );
 
-create table student_infos (
-	person_id int primary key references persons (person_id),
-	specialty_id int not null references specialties (specialty_id),
-	study_type text not null
+create table student_infos
+(
+    person_id    int primary key references persons (person_id),
+    specialty_id int  not null references specialties (specialty_id),
+    study_type   text not null
 );
 
-create table disciplines (
-	discipline_id serial primary key,
-	study_type text not null,
-	control_form text not null,
-	study_standard text not null,
-	lecture_hours int not null,
-	practice_hours int not null,
-	lab_hours int not null
+create table disciplines
+(
+    discipline_id  serial primary key,
+    study_type     text not null,
+    control_form   text not null,
+    study_standard text not null,
+    lecture_hours  int  not null,
+    practice_hours int  not null,
+    lab_hours      int  not null
 );
 
-create table specialty_disciplines (
-	specialty_id int not null references specialties (specialty_id),
-	discipline_id int not null references disciplines (discipline_id),
-	primary key (specialty_id, discipline_id)
+create table specialty_disciplines
+(
+    specialty_id  int not null references specialties (specialty_id),
+    discipline_id int not null references disciplines (discipline_id),
+    primary key (specialty_id, discipline_id)
 );
 
-create table student_semester_disciplines (
-	student_id int not null references persons (person_id),
-	semester_id int not null references semesters (semester_id),
-	discipline_id int not null references disciplines (discipline_id),
-	professor_id int not null references persons (person_id),
-	semester_counter int not null,
-	score int not null,
-	score_date timestamp not null,
-	primary key (student_id, semester_id, discipline_id)
+create table student_semester_disciplines
+(
+    student_id       int       not null references persons (person_id),
+    semester_id      int       not null references semesters (semester_id),
+    discipline_id    int       not null references disciplines (discipline_id),
+    professor_id     int       not null references persons (person_id),
+    semester_counter int       not null,
+    score            int       not null,
+    score_date       timestamp not null default current_timestamp,
+    primary key (student_id, semester_id, discipline_id)
 );
