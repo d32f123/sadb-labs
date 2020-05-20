53361eaed652e9e58d94aca8a510d1f316716ee7
Oleg Romanov <romanov-97@mail.ru>
2020-05-20 15:28:37 +0300

    Change generation PostgreSQL script

--- base_dbs/scripts/init/pg.sql
+++ base_dbs/scripts/init/pg.sql
@@ -1,10 +1,14 @@
 drop table if exists student_semester_disciplines;
+drop table if exists student_infos;
+drop table if exists semesters;
+
+drop table if exists professor_student_disciplines;
+drop table if exists students;
+drop table if exists professors;
 drop table if exists specialty_disciplines;
 drop table if exists disciplines;
-drop table if exists student_infos;
 drop table if exists persons;
 drop table if exists specialties;
-drop table if exists semesters;
 drop table if exists faculties;
 
 create table faculties
@@ -13,14 +17,12 @@ create table faculties
     faculty_name text unique not null
 );
 
-create table semesters
-(
-    semester_id serial primary key
-);
 
 create table specialties
 (
     specialty_id   serial primary key,
+    faculty_id     int not null references faculties (faculty_id),
+    study_standard text not null,
     specialty_name text unique not null
 );
 
@@ -30,23 +32,26 @@ create table persons
     last_name       text not null,
     first_name      text not null,
     patronymic_name text,
-    university_role text not null,
-    faculty_id      int  not null references faculties (faculty_id)
+    university_role text not null
 );
 
-create table student_infos
+create table students
 (
     person_id    int primary key references persons (person_id),
     specialty_id int  not null references specialties (specialty_id),
     study_type   text not null
 );
 
+create table professors
+(
+    person_id    int primary key references persons (person_id),
+    faculty_id   int not null references faculties (faculty_id)
+);
+
 create table disciplines
 (
     discipline_id  serial primary key,
-    study_type     text not null,
     control_form   text not null,
-    study_standard text not null,
     lecture_hours  int  not null,
     practice_hours int  not null,
     lab_hours      int  not null
@@ -59,14 +64,14 @@ create table specialty_disciplines
     primary key (specialty_id, discipline_id)
 );
 
-create table student_semester_disciplines
+create table professor_student_disciplines
 (
-    student_id       int       not null references persons (person_id),
-    semester_id      int       not null references semesters (semester_id),
+    student_id       int       not null references students (person_id),
     discipline_id    int       not null references disciplines (discipline_id),
-    professor_id     int       not null references persons (person_id),
+    professor_id     int       not null references professors (person_id),
+    semester         int       not null,
     semester_counter int       not null,
     score            int       not null,
     score_date       timestamp not null default current_timestamp,
-    primary key (student_id, semester_id, discipline_id)
+    primary key (student_id, professor_id, discipline_id)
 );
