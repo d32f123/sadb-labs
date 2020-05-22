ae3d353c22a99c15dee9f4ea662b95c52d252b8f
Oleg Romanov <romanov-97@mail.ru>
2020-05-23 02:24:12 +0300

    Try to fix error in generation Professor

--- generator/src/main/java/com/itmo/db/generator/persistence/impl/ProfessorPersistenceWorker.java
+++ generator/src/main/java/com/itmo/db/generator/persistence/impl/ProfessorPersistenceWorker.java
@@ -1,21 +1,36 @@
 package com.itmo.db.generator.persistence.impl;
 
 import com.itmo.db.generator.generator.Generator;
-import com.itmo.db.generator.model.entity.Professor;
+import com.itmo.db.generator.model.entity.*;
 import com.itmo.db.generator.persistence.AbstractPersistenceWorker;
 import com.itmo.db.generator.persistence.db.IdentifiableDAO;
+import com.itmo.db.generator.persistence.db.postgres.dao.*;
+import com.itmo.db.generator.persistence.db.postgres.repository.*;
 
-import java.util.List;
+import java.util.*;
 
 public class ProfessorPersistenceWorker extends AbstractPersistenceWorker<Professor, Integer> {
+    private final ProfessorPostgresRepository professorPostgresRepository;
 
-    public ProfessorPersistenceWorker(Generator generator) {
+    public ProfessorPersistenceWorker(Generator generator, ProfessorPostgresRepository professorPostgresRepository) {
         super(Professor.class, generator);
+        this.professorPostgresRepository = professorPostgresRepository;
     }
 
     @Override
     protected List<? extends IdentifiableDAO<?>> doPersist(Professor entity) {
-        return null;
+        PersonPostgresDAO personPostgresDAO = new PersonPostgresDAO(
+                this.getDependencyDAOId(Person.class, entity.getId(), PersonPostgresDAO.class), null, null, null, null
+        );
+        FacultyPostgresDAO facultyPostgresDAO = new FacultyPostgresDAO(
+                this.getDependencyDAOId(Faculty.class, entity.getFacultyId(), FacultyPostgresDAO.class), null, null
+        );
+        ProfessorPostgresDAO professorPostgresDAO = new ProfessorPostgresDAO(
+                personPostgresDAO.getId(), personPostgresDAO, facultyPostgresDAO
+        );
+
+        this.professorPostgresRepository.save(professorPostgresDAO);
+        return Collections.singletonList(professorPostgresDAO);
     }
 
     @Override
