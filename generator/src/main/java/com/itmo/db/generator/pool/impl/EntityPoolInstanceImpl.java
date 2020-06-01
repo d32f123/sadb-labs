ef85cb1f1b9b6776f5b7671b94dbd8a556646f14
Andrey Nesterov <andrey.nesterov@netcracker.com>
2020-06-01 15:23:35 +0300

    Make handleQueue singleThreaded

--- generator/src/main/java/com/itmo/db/generator/pool/impl/EntityPoolInstanceImpl.java
+++ generator/src/main/java/com/itmo/db/generator/pool/impl/EntityPoolInstanceImpl.java
@@ -25,6 +25,7 @@ public class EntityPoolInstanceImpl<T extends AbstractEntity<TId>, TId> implemen
     private int pointer;
     private final Queue<ConsumerWithMeta> consumerQueue;
     private final Class<T> entityClass;
+    private final Object handlerMontior;
 
     @Data
     @AllArgsConstructor
@@ -39,6 +40,8 @@ public class EntityPoolInstanceImpl<T extends AbstractEntity<TId>, TId> implemen
         this.consumerQueue = new ConcurrentLinkedQueue<>();
         this.entityClass = entityClass;
         this.eventBus = EventBus.getInstance();
+        this.handlerMontior = new Object();
+        new Thread(this::handleQueue).start();
         this.eventBus.subscribe(GeneratorEvent.ENTITY_GENERATED, entityClass, this::poolUpdatedCallback);
         this.eventBus.subscribe(GeneratorEvent.ENTITY_GENERATION_FINISHED, entityClass, this::poolGenerationCompleteCallback);
     }
@@ -46,22 +49,31 @@ public class EntityPoolInstanceImpl<T extends AbstractEntity<TId>, TId> implemen
     @Override
     public void request(int entitiesCount, Consumer<List<T>> callback) {
         log.debug("'{}': Got request for {} entities", entityClass, entitiesCount);
-        this.consumerQueue.add(new ConsumerWithMeta(callback, entitiesCount));
-        new Thread(this::handleQueue).start();
+        synchronized (this.consumerQueue) {
+            this.consumerQueue.offer(new ConsumerWithMeta(callback, entitiesCount));
+            synchronized (this.handlerMontior) {
+                this.handlerMontior.notify();
+            }
+        }
     }
 
-    private synchronized int getNumberOfAvailableEntities() {
+    private int getNumberOfAvailableEntities() {
         return this.pool.getActualAmount() - this.pointer;
     }
 
-    private synchronized void handleQueue() {
-        log.debug("'{}': HandleQueue running", entityClass);
+    // TODO: Оставить только Discipline, чекнуть локи
+    private void handleQueue() {
         while (true) {
-            ConsumerWithMeta consumer = consumerQueue.peek();
+            log.debug("'{}': HandleQueue iteration", entityClass);
+            ConsumerWithMeta consumer;
+            synchronized (consumerQueue) {
+                consumer = consumerQueue.peek();
 
-            if (consumer == null) {
-                log.warn("'{}': Peeked at queue but no consumer found, whoops?", entityClass);
-                return;
+                if (consumer == null) {
+                    log.trace("'{}': Peeked at queue but no consumer found", entityClass);
+                    this.waitForMonitor();
+                    continue;
+                }
             }
 
             if (this.pool.isFrozen() && this.getNumberOfAvailableEntities() < consumer.entitiesCount) {
@@ -74,7 +86,8 @@ public class EntityPoolInstanceImpl<T extends AbstractEntity<TId>, TId> implemen
             if (this.getNumberOfAvailableEntities() < consumer.entitiesCount) {
                 log.debug("'{}': Pool is not frozen and not enough entities as of yet ({} / {})",
                         entityClass, this.getNumberOfAvailableEntities(), consumer.entitiesCount);
-                return;
+                this.waitForMonitor();
+                continue;
             }
 
             log.debug("'{}': Enough entities available", entityClass);
@@ -88,17 +101,34 @@ public class EntityPoolInstanceImpl<T extends AbstractEntity<TId>, TId> implemen
         }
     }
 
+    private void waitForMonitor() {
+        try {
+            synchronized (this.handlerMontior) {
+                this.handlerMontior.wait();
+            }
+        } catch (InterruptedException ignored) {
+        }
+    }
+
     private synchronized void sendToConsumer(ConsumerWithMeta consumer, List<T> entities) {
         new Thread(() -> consumer.getConsumer().accept(entities)).start();
     }
 
     private synchronized void poolUpdatedCallback(GeneratorEventMessage<T, TId, T> message) {
-        this.handleQueue();
+        synchronized (this.pool) {
+            synchronized (this.handlerMontior) {
+                this.handlerMontior.notify();
+            }
+        }
     }
 
     // TODO: Unsub here from everything
     private void poolGenerationCompleteCallback(GeneratorEventMessage<T, TId, ?> message) {
-        this.handleQueue();
+        synchronized (this.pool) {
+            synchronized (this.handlerMontior) {
+                this.handlerMontior.notify();
+            }
+        }
     }
 
 }
